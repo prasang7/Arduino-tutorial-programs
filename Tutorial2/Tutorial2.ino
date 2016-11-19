1d2dcf8187b3d698643005b326410cd97332589e
prasang7 <prasang.sharma7@gmail.com>
2016-11-19 18:54:19 +0530

    Tutorial-2, LED switcher

--- Tutorial2/Tutorial2.ino
+++ Tutorial2/Tutorial2.ino
@@ -1,8 +1,29 @@
+//program for switching 2 LEDs (alternative blinks)
+
+int redLED = 9;
+int yellowLED = 8;
+
+int onTime = 500;
+int offTime = 500;
+
 void setup() {
-  
-  
+  pinMode(redLED, OUTPUT);
+  pinMode(yellowLED, OUTPUT);
 }
 
 void loop() {
   
+  //switch LEDs
+  digitalWrite(redLED, HIGH);
+  digitalWrite(yellowLED, LOW);
+  
+  //give some delay
+  delay(onTime);
+  
+  //switch LEDs
+  digitalWrite(redLED, LOW);
+  digitalWrite(yellowLED, HIGH);
+  
+  //give some delay
+  delay(offTime);
 }
