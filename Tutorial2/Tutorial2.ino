00bce8f8b69a4bc114e4e58d8e00e7bb50ede4eb
prasang7 <prasang.sharma7@gmail.com>
2016-11-19 19:01:03 +0530

    Tutorial, alternative LED switching

--- Tutorial2/Tutorial2.ino
+++ Tutorial2/Tutorial2.ino
@@ -1,29 +1,19 @@
-//program for switching 2 LEDs (alternative blinks)
+//program for switching LED
 
 int redLED = 9;
-int yellowLED = 8;
 
 int onTime = 500;
 int offTime = 500;
 
 void setup() {
   pinMode(redLED, OUTPUT);
-  pinMode(yellowLED, OUTPUT);
 }
 
 void loop() {
   
-  //switch LEDs
   digitalWrite(redLED, HIGH);
-  digitalWrite(yellowLED, LOW);
-  
-  //give some delay
   delay(onTime);
   
-  //switch LEDs
   digitalWrite(redLED, LOW);
-  digitalWrite(yellowLED, HIGH);
-  
-  //give some delay
   delay(offTime);
 }
